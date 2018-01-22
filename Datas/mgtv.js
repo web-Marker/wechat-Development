fb51d4b5bf1b08fe5391823056423da9fdf9572d
Mark <mark@MarkdeiMac.local>
2018-01-22 09:44:48 +0800

    What:优化readme.md
    * 简短的描述干了什么
    
    Why:
    
    * 我为什么要这么做？
    
    How:
    
    * 我是怎么做的？这么做会有什么副作用？

--- Datas/mgtv.js
+++ Datas/mgtv.js
@@ -2,7 +2,7 @@
 * @Author: mark
 * @Date:   2016-09-27 17:33:45
 * @Last Modified by:   mark
-* @Last Modified time: 2016-11-25 15:34:28
+* @Last Modified time: 2017-01-16 12:38:26
 */
 
 import Promise from 'es6-promise';
@@ -17,13 +17,13 @@ var newData = {
 
         return new Promise((resolve,reject) => {
             wx.request({
-              url: params.API_URL,
-              data:Object.assign({}, params.data),
-              header: {
-                'Content-Type': 'application/json'
-              },
-              success: resolve,
-              fail: reject 
+                url: params.API_URL,
+                data:Object.assign({}, params.data),
+                header: {
+                  'Content-Type': 'application/json'
+                },
+                success: resolve,
+                fail: reject 
             })
         })
 
